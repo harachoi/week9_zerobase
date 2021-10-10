e1f93daec99649b3adbe9dace8e9b2859e84b461
Hara Choi <hara77227722@gmail.com>
2021-10-10 09:14:08 +0900

    done

--- zerobase-homework/src/main/java/com/example/zerobase/Homework.java
+++ zerobase-homework/src/main/java/com/example/zerobase/Homework.java
@@ -5,6 +5,7 @@ import com.example.zerobase.domain.ZerobaseCourseRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,17 +17,50 @@ public class Homework {
 
     public Optional<ZerobaseCourse> getZerobaseCourse(Long id) {
         // TODO: id 가 일치하며, hidden = false 인 강의만 조회되어야 함
-        return null;
+
+        ZerobaseCourse course = repository.findById(id);
+
+        if (course != null) {
+            if (!course.isHidden())
+                return Optional.of(course);
+        }
+
+        return Optional.empty();
     }
 
     public List<ZerobaseCourse> getZerobaseCourse(String status) {
         // TODO: status가 일치하고, hidden = false 인 강의들이 조회되어야 함
-        return null;
+        List<ZerobaseCourse> list = repository.findAll();
+        if (list == null) {
+            return null;
+        }
+
+        List<ZerobaseCourse> courseList = new ArrayList<>();
+        for (ZerobaseCourse x : list) {
+            if (x.getStatus().equals(status) && !x.isHidden()) {
+                courseList.add(x);
+            }
+        }
+
+        return courseList;
     }
 
     public List<ZerobaseCourse> getOpenZerobaseCourse(LocalDate targetDt) {
         // TODO: status = "OPEN" 이고, hidden = false 이며,
-        //  startAt <= targetDt && targetDt <= endAt 인 강의만 조회되어야함.
-        return null;
+        // startAt <= targetDt && targetDt <= endAt 인 강의만 조회되어야함.
+        List<ZerobaseCourse> list = repository.findAll();
+        if (list == null) {
+            return null;
+        }
+
+        List<ZerobaseCourse> courseList = new ArrayList<>();
+        for (ZerobaseCourse x : list) {
+            if (x.getStatus().equals("OPEN") && !x.isHidden() && x.getStartAt().isBefore(targetDt)
+                    && x.getEndAt().isAfter(targetDt)) {
+                courseList.add(x);
+            }
+        }
+
+        return courseList;
     }
 }
